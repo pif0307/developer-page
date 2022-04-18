4d8cd08524fc6dadc9e53bae939325b9cd502477
Sogogi <pif0307@gmail.com>
2022-04-19 06:37:58 +0900

    initial settings

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <v-container fluid>
+      <v-row align="center">
+        <v-spacer />
+        <v-col cols="6" class="d-flex justify-center">
+          <v-btn
+            class="px-4"
+            text
+            plain
+            v-for="item in navItems"
+            :key="item"
+            :to="item.to"
+          >
+            {{ item.text }}
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-main app>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navItems: [
+        {
+          text: "Info",
+          to: "/info",
+        },
+        {
+          text: "About",
+          to: "/about",
+        },
+        {
+          text: "Contact",
+          to: "/contect",
+        },
+        {
+          text: "Projects",
+          to: "/projects",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./scss/variables.scss";
+
+v-app {
+  font-family: $body-font-family !important;
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="d-flex align-center flex-column">
+    <div style="height: 80px" />
+    <div class="text-h2 font-weight-medium">안녕하세요</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrollPosY: 0,
+    };
+  },
+
+  methods: {
+    onScroll(e) {
+      this.scrollPosY = e.target.documentElement.scrollTop;
+      console.log(this.scrollPosY);
+    },
+  },
+
+  mounted() {
+    document.addEventListener("scroll", this.onScroll);
+  },
+};
+</script>
+
+<style></style>
