811fdf56744071dd9f5f434e15504c786736c07c
Sogogi <pif0307@gmail.com>
2022-04-20 07:29:53 +0900

    feat: Add slogan text

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,5 @@
 <template>
   <v-app>
-    <v-container fluid>
-      <v-row align="center">
-        <v-spacer />
-        <v-col cols="6" class="d-flex justify-center">
-          <v-btn
-            class="px-4"
-            text
-            plain
-            v-for="item in navItems"
-            :key="item"
-            :to="item.to"
-          >
-            {{ item.text }}
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-
     <v-main app>
       <router-view />
     </v-main>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,7 +1,49 @@
 <template>
-  <div class="d-flex align-center flex-column">
-    <div style="height: 80px" />
-    <div class="text-h2 font-weight-medium">안녕하세요</div>
+  <div>
+    <div id="alpha-target">
+      <div id="background"></div>
+    </div>
+
+    <div
+      class="content d-flex align-center justify-center flex-column"
+      style="height: 100vh"
+    >
+      <div class="d-flex">
+        <div id="anim-text-parent" class="d-flex">
+          <div id="anim-text-text">
+            <div
+              class="text-h2 font-weight-bold blue--text mb-2 text--lighten-3"
+              v-for="(text, index) in texts"
+              :key="index"
+            >
+              {{ text }}
+            </div>
+          </div>
+        </div>
+        <div class="text-h2 font-weight-bold white--text">를 더욱 아름답게</div>
+      </div>
+      <div class="d-flex mt-5">
+        <div class="text-h6 font-weight-medium white--text">
+          Make The &nbsp;
+        </div>
+
+        <div id="anim-text-en-parent">
+          <div id="anim-text-en-text" class="d-flex flex-column align-center">
+            <div
+              class="text-h6 font-weight-medium blue--text text--lighten-3"
+              v-for="(text, index) in texts_en"
+              :key="index"
+            >
+              {{ text }}
+            </div>
+          </div>
+        </div>
+
+        <div class="text-h6 font-weight-medium white--text">
+          &nbsp; More Beautiful
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +52,31 @@ export default {
   data() {
     return {
       scrollPosY: 0,
+      texts: [
+        "세계",
+        "예술",
+        "사랑",
+        "인생",
+        "하늘",
+        "운명",
+        "소원",
+        "청춘",
+        "시작",
+        "신뢰",
+      ],
+
+      texts_en: [
+        "World",
+        "Art",
+        "Love",
+        "Life",
+        "Sky",
+        "Fate",
+        "Wish",
+        "Youth",
+        "Start",
+        "Trust",
+      ],
     };
   },
 
@@ -26,4 +93,92 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.content > div {
+  z-index: 1;
+}
+
+#anim-text-parent {
+  height: 70px;
+  overflow-y: hidden;
+  display: block;
+}
+
+#anim-text-text {
+  transform: translateY(-1500px);
+
+  animation: anim-text-position-keyframes 5s;
+  animation-fill-mode: forwards;
+}
+
+@keyframes anim-text-position-keyframes {
+  from {
+    transform: translateY(-1400px);
+  } /* 애니메이션 높이 */
+  to {
+    transform: translateY(0);
+  }
+}
+
+#anim-text-en-parent {
+  height: 35px;
+  overflow-y: hidden;
+  display: block;
+}
+
+#anim-text-en-text {
+  transform: translateY(-750px);
+
+  animation: anim-text-en-position-keyframes 5s;
+  animation-fill-mode: forwards;
+}
+
+@keyframes anim-text-en-position-keyframes {
+  from {
+    transform: translateY(-650px);
+  } /* 애니메이션 높이 */
+  to {
+    transform: translateY(0);
+  }
+}
+
+#alpha-target {
+  animation: 3s circle ease-out;
+  animation-fill-mode: forwards;
+  animation-delay: 0.5s;
+
+  position: fixed;
+  -webkit-clip-path: circle(0%);
+  clip-path: circle(0%);
+  mask-size: cover;
+  mask-position: center;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+}
+
+@keyframes circle {
+  0% {
+    -webkit-clip-path: circle(0%);
+    clip-path: circle(0%);
+  }
+  20% {
+    -webkit-clip-path: circle(10%);
+    clip-path: circle(10%);
+  }
+  100% {
+    -webkit-clip-path: circle(510%);
+    clip-path: circle(150%);
+  }
+}
+
+#background {
+  z-index: 0;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+}
+</style>
