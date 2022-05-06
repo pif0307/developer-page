cad58ff221773a0d68f2de3165020d5af2e99a12
Sogogi <pif0307@gmail.com>
2022-05-06 12:02:59 +0900

    feat: Add gtag to analystics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: "G-0MXFLJMKTQ" },
+});
 
 new Vue({
   router,
